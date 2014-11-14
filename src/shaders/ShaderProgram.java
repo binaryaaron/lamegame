8a1f21e9e95a35a62bb2e7402b0f75e9c3f8f477
Weston Ortiz <weston@wortiz.com>
2014-11-13 21:53:32 -0700

    Print error on shader failure

--- src/shaders/ShaderProgram.java
+++ src/shaders/ShaderProgram.java
@@ -126,8 +126,9 @@ public abstract class ShaderProgram
     GL20.glCompileShader(shaderID);
     if (GL20.glGetShader(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE)
     {
-      System.out.println("could not compile shader");
+      System.out.println(GL20.glGetShaderInfoLog(shaderID, 1000));
       System.exit(-1);
+      
 
     }
     return shaderID;
