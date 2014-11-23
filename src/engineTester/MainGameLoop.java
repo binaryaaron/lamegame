09a30414292b5f46c013870fde63cd83183f942e
hhweeks <hansel.711@gmail.com>
2014-11-23 12:30:44 -0700

    server test update

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,16 +6,23 @@
  */
 package engineTester;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import gameObjects.Asteroid;
 import models.RawModel;
 import models.TexturedModel;
+import server.WalkerClient;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import physics.PhysicsUtilities;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
@@ -28,44 +35,77 @@ import entities.Entity;
 import entities.Light;
 import world.BoxUtilities;
 
+import javax.swing.*;
+
 public class MainGameLoop
 {
 
-  public final static boolean PRINT_FPS = false;
-  private final static boolean PHYSICS_DEBUG = true;
-
-  public static void main(String[] args)
+	  public final static boolean PRINT_FPS = false;
+	  private final static boolean PHYSICS_DEBUG = true;
+	  private final static boolean ASTEROIDS =false;
+	  private final static boolean SERVER_TEST=true;
+	  private static int nAsteroids=50;
+	  static Entity player;
+  public static void main(String[] args) throws IOException
   {
+	//String ipAddress=args[0];
+	    DisplayManager.createDisplay();
+	    Loader loader = new Loader();
+	    ModelMap modelMap = new ModelMap();
 
-    DisplayManager.createDisplay();
-    Loader loader = new Loader();
-    ModelMap modelMap = new ModelMap();
+	    // create skybox, this is not an entity so it is seperate
+	    RawModel skyBox = OBJLoader.loadObjModel("SkyBox2", loader, true);
+	    ModelTexture skyTexture = new ModelTexture(loader.loadTexture("SkyBox2"));
+	    TexturedModel texturedSkyBox = new TexturedModel(skyBox, skyTexture);
+	    SkyBox skyBoxEntity = new SkyBox(loader, texturedSkyBox);
 
-    // create skybox, this is not an entity so it is seperate
-    RawModel skyBox = OBJLoader.loadObjModel("SkyBox2", loader, true);
-    ModelTexture skyTexture = new ModelTexture(
-        loader.loadTexture("SkyBox2"));
-    TexturedModel texturedSkyBox = new TexturedModel(skyBox, skyTexture);
-    SkyBox skyBoxEntity = new SkyBox(loader, texturedSkyBox);
+	    modelMap.getTexturedModelList().put("Play",
+	        modelMap.getTexturedModelList().get("S001"));
+	    // create lights and camera for the player. camera position should be set in
+	    // parsing routine
+	    Light light = new Light(new Vector3f(10f, 5f, 2000f), new Vector3f(1.0f,
+	        1.0f, 1.0f));
+	    Camera camera = new Camera();
+	    MasterRenderer renderer = new MasterRenderer(camera);
 
-    //create lights and camera for the player. camera position should be set in
-    //parsing routine
-    Light light = new Light(new Vector3f(10f, 5f, 2000f), new Vector3f(
-        1.0f, 1.0f, 1.0f));
-    Camera camera = new Camera();
-    MasterRenderer renderer = new MasterRenderer(camera);
+	    PerformanceUtilities pu = new PerformanceUtilities();
 
-    PerformanceUtilities pu = new PerformanceUtilities();
-
-    // this will be information read from a socket
-    // format: ID,x,y,z,rotx,rot,y,rotz,scale
-    String testInput;
+	    // this will be information read from a socket
+	    // format: ID,x,y,z,rotx,rot,roty,rotz,scale
+	    String testInput;
 
     if (PHYSICS_DEBUG)
     {
-      testInput = "A001,1,0,-5,0,0,0,1;" + "A002,-1,0,-5,0,0,0,0.5";
+      testInput = "A001,1,0,-20,0,0,0,1;" + "A002,-1,0,-20,0,0,0,0.5";
 
-    }
+    }else if(SERVER_TEST)
+    {
+        WalkerClient wc=null;
+        try
+        {
+          wc=new WalkerClient(args);
+
+        } catch (IOException e)
+        {
+          e.printStackTrace();
+        }
+        testInput=wc.updateClientGameState("Play,0,0,4,0,0,0,0.3;");
+
+      }
+      else if(ASTEROIDS){
+        Random rand=new Random();
+        testInput="Play,0,0,4,0,0,0,0.3;";
+        for(int i=0;i<nAsteroids;i++){
+          int a=rand.nextInt(2)+1;
+          int x=rand.nextInt(30)-15;
+          int y=rand.nextInt(30)-15;
+          int z=rand.nextInt(30)-15;
+          float s=rand.nextFloat()*2;
+        testInput=testInput.concat("A00"+a+","+x+","+y+","+z+",0,0,0,"+s+";");
+        }
+        
+        
+      }
 
     else
     {
@@ -113,103 +153,121 @@ public class MainGameLoop
     {
       pu.startFrameCounter();
     }
+
+    long startTime = System.currentTimeMillis();
+    long lastTime = System.currentTimeMillis();
+
     /* Perform object movement as long as the window exists */
     while (!Display.isCloseRequested())
     {
-      if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
-      {
-        break;
-      }
+    	 /* Perform object movement as long as the window exists */
+    	WalkerClient wc=null;
+    if(SERVER_TEST)    wc =new WalkerClient(args);
+      
+          if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+          {
+            break;
+          }
 
+          // Camera to follow player.
+
+          float camxShift;
+          float camyShift;
+          float camzShift;
+
+          // controls for physics testing with two asteroids
+          // wasd control leftest asteroid, arrows control rightmost.
+          // holding shift will slow down the shifting speed.
       //controls for physics testing with two asteroids
       //wasd control leftest asteroid, arrows control rightmost.
       //holding shift will slow down the shifting speed.
       if (PHYSICS_DEBUG)
       {
-        Float scale = 0.1f;
+        Float scale = 0.001f;
         Entity Asteroid1 = renderList.get(1);
         Entity Asteroid2 = renderList.get(0);
+
+        long time = System.currentTimeMillis();
+        if (time - lastTime > 25)
+        {
+          Asteroid1.move();
+          Asteroid2.move();
+          lastTime = time;
+          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
+          {
+            PhysicsUtilities.elasticCollision(50, Asteroid1.vel, 200,
+                Asteroid2.vel);
+          }
+        }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard
             .isKeyDown(Keyboard.KEY_RSHIFT))
         {
-          scale = 0.01f;
+          scale = 0.0001f;
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
         {
-          Asteroid2.translate(scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(-scale, 0.0f, 0f);
-          }
+          Asteroid2.vel.x +=scale;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
         {
-          Asteroid2.translate(-scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(scale, 0.0f, 0f);
-          }
+          Asteroid2.vel.x -=scale;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_UP))
         {
-          Asteroid2.translate(0.0f, scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(0.0f, -scale, 0f);
-          }
+          Asteroid2.vel.y += scale;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
         {
-          Asteroid2.translate(0.0f, -scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(0.0f, scale, 0f);
-          }
+          Asteroid2.vel.y -= scale;
+
         }
         // /
         if (Keyboard.isKeyDown(Keyboard.KEY_D))
         {
-          Asteroid1.translate(scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(-scale, 0.0f, 0f);
-          }
+          Asteroid1.vel.x += scale;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_A))
         {
-          Asteroid1.translate(-scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(scale, 0.0f, 0f);
-          }
+          Asteroid1.vel.x -= scale;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_W))
         {
-          Asteroid1.translate(0.0f, scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(0.0f, -scale, 0f);
-          }
+          Asteroid1.vel.y += scale;
+
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_S))
         {
-          Asteroid1.translate(0.0f, -scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(0.0f, scale, 0f);
-          }
+          Asteroid1.vel.y -= scale;
         }
 
       }
 
-      //camera controls
-      else
-      {
-        camera.move();
-      }
+      if(player!=null){
+      Vector3f pos=player.getPosition();
+      if(SERVER_TEST) testInput=wc.updateClientGameState("Play,"+pos.x+","+pos.y+","+pos.z+","+player.getRotX()+","+player.getRotY()+","+player.getRotZ()+","+0.3);
+      renderList=parseGameStateString(testInput,modelMap);
+      
+      
+      float yOff = 1;
+      float zOff = 3;
 
+      camxShift = ((float) Math.sin(player.getRotY() * 3.14 / 180) * zOff);// *(float)
+                                                                           // Math.sin(player.getRotZ()*3.14/180);
+      camyShift = ((float) (Math.cos(player.getRotX() * 3.14 / 180)) * yOff)
+          - (float) (Math.sin(player.getRotX() * 3.14 / 180)) * zOff;// *(float)
+                                                                     // Math.cos(player.getRotX()*3.14/180);
+      camzShift = -((float) Math.sin(player.getRotX() * 3.14 / 180) * yOff)
+          + ((float) Math.cos(player.getRotX() * 3.14 / 180) * zOff);// *(float)
+                                                                     // Math.cos(player.getRotX()*3.14/180);;
+
+      camera.setPosition(player.getPosition().x + camxShift,
+          player.getPosition().y + camyShift, player.getPosition().z
+              + camzShift);
+      camera.setRotation(-player.getRotX(), -player.getRotY(),
+          -player.getRotZ());
+      }
 			/* render each entity passed to the client */
       for (Entity ent : renderList)
       {
@@ -234,5 +292,45 @@ public class MainGameLoop
     loader.cleanUp();
     DisplayManager.closeDisplay();
   }
+    
+    
+    
+    private static List<Entity> parseGameStateString(String testInput,ModelMap modelMap){
+  	  
+  	  List<Entity> renderList = new ArrayList<>();
+  	    String[] sceneInfo = testInput.split(";");
+  	    for (String object : sceneInfo)
+  	    {
+  	      String[] currentLine = object.split(",");
+  	      String id;
+  	      float x, y, z, xr, yr, zr, s;
+  	      // translate all input data into appropriate entities;
+  	      id = currentLine[0];
+  	      x = Float.parseFloat(currentLine[1]);
+  	      y = Float.parseFloat(currentLine[2]);
+  	      z = Float.parseFloat(currentLine[3]);
+  	      xr = Float.parseFloat(currentLine[4]);
+  	      yr = Float.parseFloat(currentLine[5]);
+  	      zr = Float.parseFloat(currentLine[6]);
+  	      s = Float.parseFloat(currentLine[7]);
 
+
+  	      if (object.startsWith("Play"))
+  	      {
+
+  	        player = new Entity(modelMap.getTexturedModelList().get(id),
+  	            new Vector3f(x, y, z), xr, yr, zr, s);
+
+  	      }
+  	      else
+  	      {
+
+  	        Entity tmp_Entity = new Entity(modelMap.getTexturedModelList().get(id),
+  	            new Vector3f(x, y, z), xr, yr, zr, s);
+  	        renderList.add(tmp_Entity);
+  	      }
+  	    }
+  	    return renderList;
+    }
+    
 }
--- src/server/WalkerThread.java
+++ src/server/WalkerThread.java
@@ -1,13 +1,13 @@
 package server;
 
-import org.lwjgl.MemoryUtil;
-
 import java.awt.Point;
-
 import java.awt.image.ReplicateScaleFilter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.lang.instrument.Instrumentation;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,65 +17,35 @@ public class WalkerThread extends Thread
 { 
   public boolean printlocation=true;
   public boolean printclients=false;
-  public ServerPackage myServerPackage=null;//contains info for connected client
-  private LinkedList<ServerPackage> allPackageList=new LinkedList<>();
+  private String inputFromClient;
+//  private LinkedList<Point> playersLocationList=new LinkedList<>();
+  private String outputToClient;
   
   private Socket myClientSocket = null;
-  private final int xBound = 500;
-  private final int yBound = 500;
-  private Point reqLocation = null;
+  PrintWriter out=null;
+  BufferedReader in=null;
 
   public WalkerThread(Socket mySocket, int ID)
   {
     super("WalkerThread");
     this.myClientSocket = mySocket;
-    myServerPackage=new ServerPackage(ID);
   }
 
   public void run()
   {    
     try
     {
-      ObjectOutputStream outToClient=new ObjectOutputStream(myClientSocket.getOutputStream());
-      ObjectInputStream inFromClient=new ObjectInputStream(myClientSocket.getInputStream());
+      out=new PrintWriter(myClientSocket.getOutputStream());
+      in=new BufferedReader(new InputStreamReader(myClientSocket.getInputStream()));
       int loop = 0;
       
-      while((myServerPackage=(ServerPackage)inFromClient.readObject())!=null)
-      {        
-        reqLocation=myServerPackage.currentP;
-        if(printlocation)System.out.println("Client("+reqLocation.x+","+reqLocation.y+")");
-        if(printlocation)System.out.println("loop " + loop);
-        
-        if ((reqLocation.x > 0) && (reqLocation.x < xBound) && (reqLocation.y > 0) && (reqLocation.y < yBound))
-        {
-          //if program gets here myServerPackage.currentP is in bounds, no change needed
-        }
-        else
-        {//else new location is out of bounds, revert to previous location
-          myServerPackage.currentP.x=myServerPackage.prevP.x;//if the location is out of bounds, pop it off the list, leaving the last valid location
-          myServerPackage.currentP.y=myServerPackage.prevP.y;
-        }
-        
-        ////send ServerPackages of all clients to this client
-        allPackageList.clear();
-        
-        
-        synchronized (myServerPackage)
-        {
-          for(WalkerThread wt:WalkerServer.threadList)
-          {
-            allPackageList.add(wt.myServerPackage);
-          }
-        }
-        allPackageList.addFirst(myServerPackage);
-        // outToClient.writeObject(allPackageList);
-        if(printclients)System.out.println("hosts connected: "+allPackageList.size());
-        System.out.println("size of allPackageList"+ server.MemoryUtil.shallowSizeOf(allPackageList));
-        outToClient.writeUnshared(allPackageList);
-
+      while((inputFromClient=in.readLine())!=null)
+      {
         loop++;
-      }
-      
+        //for initial server test, call the getter method to update from this loop
+        outputToClient=inputFromClient;//test version, this is not testing or updating the input, just bouncing it
+        out.println(outputToClient);
+      }      
     } 
     catch (java.net.SocketException e)
     {
@@ -91,9 +61,16 @@ public class WalkerThread extends Thread
     {
       e.printStackTrace();
     }
-    catch (NumberFormatException | ClassNotFoundException e)
+    catch (NumberFormatException e)
     {
       e.printStackTrace();
     }
+  }
+  
+  public String updateServerGameState(String updateString)//TODO maybe call this a getter
+  {
+    outputToClient=updateString;
+    out.println(outputToClient);
+    return inputFromClient;
   }
 }
