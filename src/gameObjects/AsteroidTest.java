e528b37eac40375bc89c4498bd1f7226c826a07f
Aaron Gonzales <xysmas@gmail.com>
2014-11-22 21:04:57 -0700

    asteroid passes basic tests

---
+++ src/gameObjects/AsteroidTest.java
@@ -0,0 +1,42 @@
+package gameObjects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AsteroidTest
+{
+  Asteroid ass;
+
+  @Before public void setUp() throws Exception
+  {
+    ass = new Asteroid();
+
+  }
+
+  @After public void tearDown() throws Exception
+  {
+
+  }
+
+  @Test public void testUponDeath() throws Exception
+  {
+    ass.damageObject(100);
+    assertTrue((ass.getHitPoints()) > 0 );
+    ass.damageObject(100);
+    assertTrue((ass.getHitPoints()) > 0 );
+    ass.damageObject(100);
+    assertTrue((ass.getHitPoints()) > 0 );
+    ass.damageObject(100);
+    assertTrue((ass.getHitPoints()) > 0 );
+    ass.damageObject(100);
+    assertFalse((ass.getHitPoints()) > 0);
+  }
+
+  @Test public void testAsteroidSize() throws Exception
+  {
+    System.out.println("Asteroid Size: " + ass.getBBox().getMax());
+  }
+}
