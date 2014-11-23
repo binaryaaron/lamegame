e3e1100d074af74d82c6f2f096d5d1eac7220e4e
emsubdog/em <rnicholson@unm.edu>
2014-11-23 11:35:44 -0700

    added a font.png

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -26,213 +26,163 @@ import toolbox.PerformanceUtilities;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
-import world.BoxUtilities;
 
-public class MainGameLoop
+public class MainGameLoop 
 {
 
-  public final static boolean PRINT_FPS = false;
-  private final static boolean PHYSICS_DEBUG = true;
+	public final static boolean PRINT_FPS = false;
+	private final static boolean PHYSICS_DEBUG = false;
 
-  public static void main(String[] args)
-  {
+	public static void main(String[] args) 
+	{
 
-    DisplayManager.createDisplay();
-    Loader loader = new Loader();
-    ModelMap modelMap = new ModelMap();
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		ModelMap modelMap = new ModelMap();
+		
+		
+		// create skybox, this is not an entity so it is seperate
+		RawModel skyBox = OBJLoader.loadObjModel("SkyBox2", loader, true);
+		ModelTexture skyTexture = new ModelTexture(
+				loader.loadTexture("SkyBox2"));
+		TexturedModel texturedSkyBox = new TexturedModel(skyBox, skyTexture);
+		SkyBox skyBoxEntity = new SkyBox(loader, texturedSkyBox);
 
-    // create skybox, this is not an entity so it is seperate
-    RawModel skyBox = OBJLoader.loadObjModel("SkyBox2", loader, true);
-    ModelTexture skyTexture = new ModelTexture(
-        loader.loadTexture("SkyBox2"));
-    TexturedModel texturedSkyBox = new TexturedModel(skyBox, skyTexture);
-    SkyBox skyBoxEntity = new SkyBox(loader, texturedSkyBox);
+		
+		//create lights and camera for the player. camera position should be set in 
+		//parsing routine
+		Light light = new Light(new Vector3f(10f, 5f, 2000f), new Vector3f(
+				1.0f, 1.0f, 1.0f));
+		Camera camera = new Camera();
+		MasterRenderer renderer = new MasterRenderer(camera);
 
-    //create lights and camera for the player. camera position should be set in
-    //parsing routine
-    Light light = new Light(new Vector3f(10f, 5f, 2000f), new Vector3f(
-        1.0f, 1.0f, 1.0f));
-    Camera camera = new Camera();
-    MasterRenderer renderer = new MasterRenderer(camera);
+		PerformanceUtilities pu = new PerformanceUtilities();
 
-    PerformanceUtilities pu = new PerformanceUtilities();
+		// this will be information read from a socket
+		// format: ID,x,y,z,rotx,rot,y,rotz,scale
+		String testInput;
 
-    // this will be information read from a socket
-    // format: ID,x,y,z,rotx,rot,y,rotz,scale
-    String testInput;
+		if (PHYSICS_DEBUG) {
+			testInput = "A001,1,0,-5,0,0,0,0.5;" + "A002,-1,0,-5,0,0,0,0.5";
 
-    if (PHYSICS_DEBUG)
-    {
-      testInput = "A001,1,0,-5,0,0,0,1;" + "A002,-1,0,-5,0,0,0,0.5";
+		}
 
-    }
+		else {
+			testInput = "S001,0,0,-20,0,0,0,0.01;" + "S002,0,15,-20,0,0,0,0.3;"
+					+ "A001,4,2,-3,0,0,0,1;"+"Cam,0,0,3,0,90,0,1";
+		}
 
-    else
-    {
-      testInput = "S001,0,0,-20,0,0,0,0.01;" + "S002,0,15,-20,0,0,0,0.3;"
-          + "A001,4,2,-3,0,0,0,1;" + "Cam,0,0,3,0,90,0,1";
-    }
+		List<Entity> renderList = new ArrayList<>();
 
-    List<Entity> renderList = new ArrayList<>();
+		String[] sceneInfo = testInput.split(";");
+		for (String object : sceneInfo) {
+			String[] currentLine = object.split(",");
+			String id;
+			float x, y, z, xr, yr, zr, s;
+			// translate all imput data into appropriate entities;
+			x = Float.parseFloat(currentLine[1]);
+			y = Float.parseFloat(currentLine[2]);
+			z = Float.parseFloat(currentLine[3]);
+			xr = Float.parseFloat(currentLine[4]);
+			yr = Float.parseFloat(currentLine[5]);
+			zr = Float.parseFloat(currentLine[6]);
+			s = Float.parseFloat(currentLine[7]);
+			System.out.println(object.charAt(0));
+			if(object.startsWith("Cam")){
+			camera.setPosition(new Vector3f(x, y, z));
+			camera.setPitch(xr);
+			camera.setYaw(yr);
+			camera.setRoll(zr);
+			}
+			
+			else{
+			
 
-    String[] sceneInfo = testInput.split(";");
-    for (String object : sceneInfo)
-    {
-      String[] currentLine = object.split(",");
-      String id;
-      float x, y, z, xr, yr, zr, s;
-      // translate all imput data into appropriate entities;
-      x = Float.parseFloat(currentLine[1]);
-      y = Float.parseFloat(currentLine[2]);
-      z = Float.parseFloat(currentLine[3]);
-      xr = Float.parseFloat(currentLine[4]);
-      yr = Float.parseFloat(currentLine[5]);
-      zr = Float.parseFloat(currentLine[6]);
-      s = Float.parseFloat(currentLine[7]);
-      System.out.println(object.charAt(0));
-      if (object.startsWith("Cam"))
-      {
-        camera.setPosition(new Vector3f(x, y, z));
-        camera.setPitch(xr);
-        camera.setYaw(yr);
-        camera.setRoll(zr);
-      }
+			id = currentLine[0];
+			
+			Entity tmp_Entity = new Entity(modelMap.getTexturedModelList().get(
+					id), new Vector3f(x, y, z), xr, yr, zr, s);
+			renderList.add(tmp_Entity);
+			}
+		}
 
-      else
-      {
+		if (PRINT_FPS) {
+			pu.startFrameCounter();
+		}
+		/* Perform object movement as long as the window exists */
+		while (!Display.isCloseRequested()) {
+			if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+				break;
+			}
 
-        id = currentLine[0];
+			
+			//controls for physics testing with two asteroids
+			//wasd control leftest asteroid, arrows control rightmost.
+			//holding shift will slow down the shifting speed.
+			if (PHYSICS_DEBUG) {
+				Float scale=0.1f;
+				Entity Asteroid1 = renderList.get(1);
+				Entity Asteroid2 = renderList.get(0);
 
-        Entity tmp_Entity = new Entity(modelMap.getTexturedModelList().get(
-            id), new Vector3f(x, y, z), xr, yr, zr, s);
-        renderList.add(tmp_Entity);
-      }
-    }
+				if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)||Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+					scale=0.01f;
+				}
+				
+				if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+					Asteroid2.translate(scale, 0.0f, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+					Asteroid2.translate(-scale, 0.0f, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+					Asteroid2.translate(0.0f, scale, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+					Asteroid2.translate(0.0f, -scale, 0f);
+				}
+				// /
+				if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+					Asteroid1.translate(scale, 0.0f, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+					Asteroid1.translate(-scale, 0.0f, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+					Asteroid1.translate(0.0f, scale, 0f);
+				}
+				if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+					Asteroid1.translate(0.0f, -scale, 0f);
+				}
 
-    if (PRINT_FPS)
-    {
-      pu.startFrameCounter();
-    }
-    /* Perform object movement as long as the window exists */
-    while (!Display.isCloseRequested())
-    {
-      if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
-      {
-        break;
-      }
+			}
 
-      //controls for physics testing with two asteroids
-      //wasd control leftest asteroid, arrows control rightmost.
-      //holding shift will slow down the shifting speed.
-      if (PHYSICS_DEBUG)
-      {
-        Float scale = 0.1f;
-        Entity Asteroid1 = renderList.get(1);
-        Entity Asteroid2 = renderList.get(0);
-
-        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard
-            .isKeyDown(Keyboard.KEY_RSHIFT))
-        {
-          scale = 0.01f;
-        }
-
-        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
-        {
-          Asteroid2.translate(scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(-scale, 0.0f, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
-        {
-          Asteroid2.translate(-scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(scale, 0.0f, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_UP))
-        {
-          Asteroid2.translate(0.0f, scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(0.0f, -scale, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
-        {
-          Asteroid2.translate(0.0f, -scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid2.translate(0.0f, scale, 0f);
-          }
-        }
-        // /
-        if (Keyboard.isKeyDown(Keyboard.KEY_D))
-        {
-          Asteroid1.translate(scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(-scale, 0.0f, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_A))
-        {
-          Asteroid1.translate(-scale, 0.0f, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(scale, 0.0f, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_W))
-        {
-          Asteroid1.translate(0.0f, scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(0.0f, -scale, 0f);
-          }
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_S))
-        {
-          Asteroid1.translate(0.0f, -scale, 0f);
-          if (BoxUtilities.collision(Asteroid1.getBox(), Asteroid2.getBox()))
-          {
-            Asteroid1.translate(0.0f, scale, 0f);
-          }
-        }
-
-      }
-
-      //camera controls
-      else
-      {
-        camera.move();
-      }
+			//camera controls
+			else {
+				camera.move();
+			}
 
 			/* render each entity passed to the client */
-      for (Entity ent : renderList)
-      {
-        renderer.processEntity(ent);
+			for (Entity ent : renderList) {
+				renderer.processEntity(ent);
 
-      }
+			}
 
-      renderer.processSkyBox(skyBoxEntity);
-      renderer.render(light, camera);
+			
+			renderer.processSkyBox(skyBoxEntity);
+			renderer.render(light, camera);
 
-      DisplayManager.updateDisplay();
-      if (PRINT_FPS)
-      {
-        pu.updateFPS();
+			DisplayManager.updateDisplay();
+			if (PRINT_FPS) {
+				pu.updateFPS();
 
-        System.out.println(pu.getFPS());
-      }
+				System.out.println(pu.getFPS());
+			}
 
-    }
+		}
 
-    renderer.cleanUp();
-    loader.cleanUp();
-    DisplayManager.closeDisplay();
-  }
+		renderer.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+	}
 
 }
