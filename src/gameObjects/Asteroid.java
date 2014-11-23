c94369bfd8f642acffb89bfe09acf4d29e1d3354
Aaron Gonzales <xysmas@gmail.com>
2014-11-22 21:03:19 -0700

    finshed first version of asteroid class

--- src/gameObjects/Asteroid.java
+++ src/gameObjects/Asteroid.java
@@ -1,76 +1,53 @@
 package gameObjects;
 
 import org.lwjgl.util.vector.Vector3f;
+import world.BoundingBox;
+
+import java.util.Random;
+
+// bounding box sizes
+// create from bigger asteroid
+// hit points
+
+
 /**
- * Created by carlyhendrickson on 11/18/14.
+ * Asteroid is the main asteroid class, obviously.
+ * It is a concrete implementation of GameObject and can be extended.
+ * Created by aarongonzales on 11/18/14.
  */
-public class Asteroid implements GObject
+public class Asteroid extends GameObject
 {
-  private Vector3f position = new Vector3f();
-  private Vector3f velocity = new Vector3f();
-  private Vector3f accel = new Vector3f();
-  private Boolean model = false;
   private static short ASTEROID_COUNT = 0;
   private static Boolean DEBUG = true;
 
-  @Override public Vector3f getPosition()
-  {
-    return position;
-  }
 
-  @Override public void setPosition(Vector3f position)
+  public Asteroid()
   {
-    this.position = position;
-  }
+    // intializes asteroid with between 100 and 1000 HP
+    this.hitPoints = Globals.randInt(1000,100);
+    ASTEROID_COUNT++;
+    this.velocity = new Vector3f(0,0,0);
+    // position should be random within the game board
+    this.position = new Vector3f(Globals.randInt(),Globals.randInt(),Globals.randInt());
+    int tmpSize = Globals.randInt(15, 4);
+    this.bbox = new BoundingBox(position, tmpSize, tmpSize, tmpSize);
+    this.model = true;
+    this.hitPoints = 500;
 
-  @Override public Vector3f getVelocity()
-  {
-    return velocity;
-  }
-
-  public void setVelocity(Vector3f velocity)
-  {
-    this.velocity = velocity;
   }
 
   /**
-   * Updates the velocity by a constant in all directions
-   *
-   * @param increase the amount to increase the velocity by. may be negative.
-   *                 must be within range of a byte (-127 -- 127)
+   * Kills this object. Should add functionality to break the asteroids in
+   * small chunks - currently unimplemented
+   * @param go the game object to die
    */
-  @Override public void increaseVelocity(byte increase)
+  @Override
+  protected void uponDeath(GameObject go)
   {
-    velocity.set(velocity.getX()+increase, velocity.getY() + increase, velocity.getZ() + increase);
+    System.out.println("asteroid down! make a new one?");
+
   }
 
-  @Override public Vector3f getAccel()
-  {
-    return accel;
-  }
 
-  public void setAccel(Vector3f accel)
-  {
-    this.accel = accel;
-  }
-
-  /**
-   * Placeholder method for getting the player's "model" - my generic
-   * term for the skin this player has at the moment
-   * @return the current skin
-   */
-  @Override public Boolean getModel()
-  {
-    return model;
-  }
-
-  /**
-   * Placeholder method for updating the player's "model" - my generic
-   * term for the skin this player has at the moment
-   * @return the current skin
-   */
-  @Override public void setModel(Boolean model)
-  {
-    this.model = model;
-  }
 }
+
