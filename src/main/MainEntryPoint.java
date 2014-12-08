19ff7754bce722b41df35caacf15a65bce6c6fc5
Aaron Gonzales <xysmas@gmail.com>
2014-12-08 16:30:03 -0700

    updated main entry point to take proper args

--- src/main/MainEntryPoint.java
+++ src/main/MainEntryPoint.java
@@ -3,21 +3,50 @@ package main;
 import engineTester.MainLoopClient;
 import engineTester.MainLoopServer;
 
+import java.io.File;
+
 /**
  * Provides a main entry point for our game. Will take args from the
- * JVM to denote if this is a server instance. mostly pseudocode for now
+ * JVM to denote if this is a server instance and an address for a server.
  */
 public class MainEntryPoint
 {
   public static void main(String[] args)
   {
-    if (args[0] == "server")
+    System.out.println(new File(".").getAbsoluteFile());
+
+    System.out.println("Args = " + args);
+    for (String s : args)
     {
-      new MainLoopServer(args);
+      System.out.println(s);
     }
+    if (args.length <= 0)
+    {
+      System.out.println("Please enter either 'server' or 'client' to start the game");
+    }
+    else if (args[0].equals("server"))
+    {
+      if (args.length == 2)
+      {
+        System.out.println("Starting server on the following machine: " + args[1]);
+        new MainLoopServer(args);
+      }
+      else
+      {
+        System.out
+            .println("Starting server on localhost ");
+        new MainLoopServer(args);
+      }
+    }
+  else if (args[0].equals("client"))
+  {
+    System.out.println("Starting client");
+    new MainLoopClient(args);
+  }
     else
     {
-      new MainLoopClient(args);
+      System.out.println("no args; Exiting");
     }
+    System.out.println("Exiting program");
   }
 }
