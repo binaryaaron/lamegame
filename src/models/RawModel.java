9610e73f2f324b052fca6eb3306a35dc238a5625
emsubdog/em <rnicholson@unm.edu>
2014-12-09 00:49:20 -0700

    commented RawModel

--- src/models/RawModel.java
+++ src/models/RawModel.java
@@ -2,38 +2,50 @@ package models;
 
 import world.BoundingBox;
 
-public class RawModel {
+public class RawModel 
+{
 	private int vaoID;
 	private int vertexCount;
 	private BoundingBox box;
-	public RawModel(int vaoIDin, int vertexCountIn, BoundingBox box){
+	/**
+	 * Create a RawModel with an ID, vertex count and bounding box
+	 * @param vaoIDin
+	 * @param vertexCountIn
+	 * @param box
+	 */
+	public RawModel(int vaoIDin, int vertexCountIn, BoundingBox box)
+	{
 		vaoID=vaoIDin;
 		vertexCount=vertexCountIn;
 		this.box = box;
-		
 	}
 
+	/**
+	 * Getters and setters
+	 * @return
+	 */
 	public BoundingBox getBoundingBox()
 	{
 		return box;
 	}
 
-	public int getVaoID() {
+	public int getVaoID() 
+	{
 		return vaoID;
 	}
 
-	public void setVaoID(int vaoID) {
+	public void setVaoID(int vaoID) 
+	{
 		this.vaoID = vaoID;
 	}
 
-	public int getVertexCount() {
+	public int getVertexCount() 
+	{
 		return vertexCount;
 	}
 
-	public void setVertexCount(int vertexCount) {
+	public void setVertexCount(int vertexCount) 
+	{
 		this.vertexCount = vertexCount;
 	}
-	
-	
-
 }
