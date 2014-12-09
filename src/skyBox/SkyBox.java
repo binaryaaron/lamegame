aca32e278b8d0c09cb50914ac25315eebc12bbb3
Weston Ortiz <wortiz@cs.unm.edu>
2014-12-09 15:23:23 -0700

    SkyBox and a few more deletes

--- src/skyBox/SkyBox.java
+++ src/skyBox/SkyBox.java
@@ -8,50 +8,55 @@ import models.TexturedModel;
 import renderEngine.Loader;
 import textures.ModelTexture;
 
+/**
+ * SkyBox class for generic sykbox usage
+ */
 public class SkyBox {
-
-	
 
 	private TexturedModel model;
 	private Loader loader;
 	private Entity skyEntity;
-	
-	public SkyBox(Loader loader,TexturedModel model){
 
-		
+	/**
+	 * Create a skybox
+	 * @param loader loader to use
+	 * @param model model to use
+	 */
+	public SkyBox(Loader loader,TexturedModel model){
 		this.model=model;
 		this.loader=loader;
 		setSkyEntity(new Entity(model.getId(), model, new Vector3f(0f,0f,0f), 0f,
 				0f, 0f,100000f));
-		
 	}
 
-
+	/**
+	 * Get the skybox model
+	 * @return model
+	 */
 	public TexturedModel getModel() {
 		return model;
 	}
 
-
+	/**
+	 * Set the skybox model
+	 * @param model model to set
+	 */
 	public void setModel(TexturedModel model) {
 		this.model = model;
 	}
 
-
-	public Loader getLoader() {
-		return loader;
-	}
-
-
-	public void setLoader(Loader loader) {
-		this.loader = loader;
-	}
-
-
+	/**
+	 * Get the entity representing the Sky (for rendering)
+	 * @return skybox entity
+	 */
 	public Entity getSkyEntity() {
 		return skyEntity;
 	}
 
-
+	/**
+	 * Set the skybox entity (for rendering)
+	 * @param skyEntity entity to set
+	 */
 	public void setSkyEntity(Entity skyEntity) {
 		this.skyEntity = skyEntity;
 	}
